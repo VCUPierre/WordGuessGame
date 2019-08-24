634eadef44da1257aec7fc6d4430603dce81f3d0
Tayvon Randall <tayvonrandall@LC02X43K7JHD2.local>
2019-08-24 14:30:42 -0400

    final commit

--- scripts.js
+++ scripts.js
@@ -1,19 +1,25 @@
 //alert("helloworld");
+var confettiSettings = { target: 'my-canvas' };
+var confetti = new ConfettiGenerator(confettiSettings);
+confetti.render();
+
 const log = document.querySelector('#letters')
 const guessHolder = document.querySelector('#Guesses');
 var inputArray = [];
 var wordArray = [];
-var guesses = 15;
+var counter = false;
 
 createWordBoxes();
-guessHolder.textContent = guesses;
-document.addEventListener('keyup', logKey);
+//document.addEventListener('keyup', logKey);
+var guesses = guessHolder.textContent;
 
 function createWordBoxes() {
     var word = randomWord(); 
+    var guesses = word.length + 3;
     for (let i = 0; i < word.length; i++){
-        wordArray.push(word[i]);
+        wordArray.push(word[i].toUpperCase());
     }
+    guessHolder.textContent = guesses;
     console.log("word array " + wordArray);
     var numberOfBoxes = word.length;
     let ui = document.querySelector("#wordBoxes");
@@ -43,22 +49,62 @@ function randomWord(){
     return carArray[x];
 
 }
+function GameOver(guessNum){
+    if(guessNum == 0){
+        if(confirm("Gameover! Play again?")){
+            document.location.reload();
+        }
+    }
+}
+function YouWin(array1, array2){
+    /*if (array1.includes(array2)){
+        alert("Win");
+    }*/
+    let wordBoxArray = [];
+    let ui = document.querySelector("#wordBoxes");
+    //alert(ui.childNodes.length);
+    for (let i = 0; i < ui.childNodes.length; i++){
+        if (ui.childNodes[i].nodeName == "LI"){
+            wordBoxArray.push(ui.childNodes[i].textContent);
+            //console.log("test"+wordBoxArray);
+        }
+    }
+    if (!wordBoxArray.includes("_")){
+        var element = document.getElementById("my-canvas");
+        element.classList.remove("display0");
+        setTimeout(function(){ document.location.reload(); }, 5000);
+        //alert("You won the car was" + word);
+        /*if(confirm("You won! Play again")){
+            document.location.reload();
+        }*/
+    }
+}
 
-function logKey(e) {
+
+document.onkeyup = function (e) {
     var inputLetter = String.fromCharCode(e.keyCode);
-    //alert(inputLetter);
+    guesses--;
+    GameOver(guesses);
+    guessHolder.textContent = guesses;
     log.textContent += ` ${inputLetter}`;
-    inputArray.push(inputLetter);
+    
     //let currentInputLetter = 
-    if (wordArray.includes(inputLetter)){
-        alert(inputArray);
+    if (wordArray.includes(inputLetter) && inputArray.includes(inputLetter)){
+        inputArray.push(inputLetter);
+        //alert(inputArray);
+        let wordIndex = wordArray.lastIndexOf(inputLetter);
+        let liIndex = document.getElementById(wordIndex);
+        liIndex.textContent = inputLetter;
+        //counter = true;     
+    } else if (wordArray.includes(inputLetter)){
+        inputArray.push(inputLetter);
         let wordIndex = wordArray.indexOf(inputLetter);
         let liIndex = document.getElementById(wordIndex);
         liIndex.textContent = inputLetter;
-        
-        //alert("success");
+    } else{
+        //do nothing 
     }
-    guesses--;
-    guessHolder.textContent = guesses;
+    YouWin(inputArray,wordArray);
+        //alert("success");
     console.log("input array "+inputArray);
 }
